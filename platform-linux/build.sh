2f712c7dccef26a6160c1f35f72dfd0369df7b4b
StephanusVictor <sjv000@gmail.com>
2022-06-25 10:01:32 +0200

    removed non-mqtt drivers

--- platform-linux/build.sh
+++ platform-linux/build.sh
@@ -13,34 +13,14 @@ cd ..
 mkdir bin
 mkdir bin-wine
 
-cd src/lib60870.netcore
-dotnet publish --no-self-contained --runtime $ARG1 -p:PublishReadyToRun=true -c Release -o ../../bin/
-
-cd ../OPC-UA-Client
-dotnet publish --no-self-contained --runtime $ARG1 -p:PublishReadyToRun=true -c Release -o ../../bin/
-
-# Dnp3Client is Windows-only (must run under Wine on Linux)
-cd ../dnp3/Dnp3Client
-dotnet publish --self-contained --runtime win-x64 -p:PublishReadyToRun=false -c Release -o ../../../bin-wine/ Dnp3Client.csproj
-
 export GOBIN=~/json-scada/bin
 go env -w GO111MODULE=auto
 
-cd ../../calculations
+cd src/calculations
 go get ./...
 go build
 cp calculations ../../bin/
 
-# cd ../plc4x-client
-# go get "github.com/icza/bitio"
-# go get ./... 
-# go build 
-# cp plc4x-client ../../bin/
-
-cd ../i104m
-go get ./...
-go build
-cp i104m ../../bin/
 
 cd ../cs_data_processor
 npm install
@@ -48,17 +28,11 @@ cd ../cs_custom_processor
 npm install
 cd ../grafana_alert2event
 npm install
-cd ../demo_simul
-npm install
 cd ../server_realtime
 npm install
 cd ../server_realtime_auth
 npm install
 cd ../updateUser
-npm install
-cd ../oshmi2json
-npm install
-cd ../oshmi_sync
 npm install
 cd ../htdocs-admin
 npm install
@@ -70,8 +44,6 @@ npm install
 cd ../telegraf-listener
 npm install
 cd ../mqtt-sparkplug
-npm install
-cd ../OPC-UA-Server
 npm install
 cd ../carbone-reports
 npm install
--- src/demo_simul/README.md
+++
@@ -1,5 +0,0 @@
-## Demo Simulator
-
-This script can simulate events, values, respond to commands combined with the default demo database.
-
-Requires Node.js.
